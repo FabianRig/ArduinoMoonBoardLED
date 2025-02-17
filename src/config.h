a3580069cae7ce73fac469b4c676a9477dd22074
Taner Kucukyuruk <tanerkay@protonmail.com>
2025-02-17 15:29:50 +0100

    fix typo

--- src/config.h
+++ src/config.h
@@ -12,7 +12,7 @@
 #define AUTO_TURN_OFF false // Set to true if you want the LEDs to turn off after inactivity
 #define AUTO_TURN_OFF_MINUTES 30 // Set the number of minutes after which the LEDs should turn off if there is no activity
 
-// Settings below ususally don't need to be adjusted unless you have a special situation
+// Settings below usually don't need to be adjusted unless you have a special situation
 
 // Number of LEDs in the LED strip (usually 150 for MoonBoard Mini, 200 for a standard MoonBoard)
 // Adjust only if you have a special setup
